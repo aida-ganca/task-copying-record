645d64c2ce32abe1e9a7eef05f3b47055712f3f7
aida-ganca <18611861pug0vka1861>
2019-08-05 13:44:39 +0300

    AddESM
    Add

--- ClassCopyAll/ClassCopyAll/AllCopyImplement.cs
+++ ClassCopyAll/ClassCopyAll/AllCopyImplement.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Terrasoft.Core;
 using Terrasoft.Core.DB;
+using Terrasoft.Core.Entities;
 
 namespace ClassCopyAll
 {
@@ -46,8 +51,48 @@ namespace ClassCopyAll
 
             return string.Empty;
         }
-       
+        public void DoCopyESM(Guid RecordId)
 
+        {
+
+            var opportunityManager = _userConnection.EntitySchemaManager.GetInstanceByName("UsrSale");
+            var nameCopy = opportunityManager.CreateEntity(_userConnection);
+            var copedName = opportunityManager.CreateEntity(_userConnection);
+            bool exist = nameCopy.FetchFromDB(new Dictionary<string, object>()
+            {
+                { "Id", RecordId }
+            });
+            if (exist)
+            {
+                copedName.SetDefColumnValues();
+                copedName.SetColumnValue("UsrName", nameCopy.GetColumnValue("UsrName"));
+                copedName.SetColumnValue("UsrNotes", nameCopy.GetColumnValue("UsrNotes"));
+                copedName.SetColumnValue("UsrLookup1Id", nameCopy.GetColumnValue("UsrLookup1Id"));
+                copedName.Save();
+                var detailData = new EntitySchemaQuery(_userConnection.EntitySchemaManager, "UsrGenre");
+                detailData.AddColumn("UsrOrder.Id");
+                detailData.AddColumn("UsrCirculation");
+                var esqFunction = detailData.CreateFilterWithParameters(FilterComparisonType.Equal,
+                   "UsrOrder.Id",
+                   RecordId);
+                detailData.Filters.Add(esqFunction);
+                var entitydetailDataColumn = detailData.GetEntityCollection(_userConnection);
+                var opportunityManager1 = _userConnection.EntitySchemaManager.GetInstanceByName("UsrGenre");//созд
+                foreach (var entity in entitydetailDataColumn)
+
+                {
+
+                    var detailsDataSet = opportunityManager1.CreateEntity(_userConnection);//создать строчку.
+                    detailsDataSet.SetDefColumnValues();
+                    detailsDataSet.SetColumnValue("UsrOrderId", copedName.GetColumnValue("Id"));
+                    detailsDataSet.SetColumnValue("UsrCirculation", entity.GetColumnValue("UsrCirculation"));
+                    detailsDataSet.Save();
+
+                }
+
+            }
+
+        }
     }
 
 }
